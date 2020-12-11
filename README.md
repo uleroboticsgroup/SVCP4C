0e758621626112d7d9a21a2aaab19cc83bfb3063
Gonzalo Esteban <gestc@unileon.es>
2020-12-11 10:29:32 +0000

    Updated README, added license, gitignore and header comments in the script file

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# SVCP4C
-Sonar Vulnerable Code Prospector For C (SVCP4C)
+# SonarCloud Vulnerable Code Prospector for C (SVCP4C)
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+A tool that aims to collect vulnerable source code from open-source repositories linked to [SonarCloud](https://sonarcloud.io/) by using its [REST API](https://sonarcloud.io/web_api). The output consists of a set of tagged files suitable for extracting features and creating training datasets for Machine Learning algorithms. Vulnerabilities are listed in each file using comments appended at the end of each file. Such comments follow the format `// starting_line,starting_offset;ending_line,ending_offset` (with offset being the column). For example:
+
+```c
+//						↓↓↓VULNERABLE LINES↓↓↓
+
+// 106,8;106,15
+
+// 126,8;126,15
+
+// 891,24;891,31
+
+// 897,24;897,31
+
+// 917,20;917,27
+```
+
+For a detailed explanation about SVCP4C, please check [this](https://doi.org/10.3390/app10041270) research paper. Also, sample datasets obtained with it are available in [this](https://github.com/uleroboticsgroup/SVCP4C) other repository.
+
+# Usage
+
+```bash
+python SVCP4C.py <output_path> <optional_args>
+```
+
+By default, the tool runs in quiet mode.
+
+## Arguments
+
+Only the first argument (`output_path`) is required and it corresponds to the directory in which the tagged vulnerable source code will be downloaded into. The rest are optional:
+
+| Argument | Description              |
+| :------: | :----------------------- |
+| -h       | Prints the usage message |
+| -v       | Executes in verbose mode |
+
+## Dependencies
+
+Running SVCP4C requires the `requests` Python package, which may be installed using pip.
+
+# Reference
+
+For scientific publications, please reference SVCP4C using:
+
+## Plain
+
+```
+Raducu, R., Esteban, G., Rodríguez Lera, F. J., & Fernández, C. (2020). Collecting Vulnerable Source Code from Open-Source Repositories for Dataset Generation. Applied Sciences, 10 (4), 1270. DOI: https://doi.org/10.3390/app10041270
+```
+
+## BibTeX
+
+```BibTeX
+@ARTICLE{Raducu2020,
+  Title     = {Collecting Vulnerable Source Code from Open-Source Repositories for Dataset Generation},
+  Author    = {Raducu, Razvan and Esteban, Gonzalo and Rodr{\'\i}guez Lera, Francisco Javier and Fern{\'a}ndez, Camino},
+  Journal   = {Applied Sciences},
+  Volume    = {10},
+  Number    = {4},
+  Pages     = {1270},
+  Year      = {2020},
+  Publisher = {Multidisciplinary Digital Publishing Institute},
+  Doi       = {https://doi.org/10.3390/app10041270}
+}
+```
+
+# License
+
+SVCP4C is licensed under [GNU GPLv3](https://choosealicense.com/licenses/gpl-3.0/).
--- SVCP4C.py
+++ SVCP4C.py
@@ -1,90 +1,108 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
-@Author Razvan Raducu
+---------------------------------------------------------------------
+SonarCloud Vulnerable Code Prospector for C (SVCP4C)
+
+A tool that aims to collect vulnerable source code from open source
+repositories linked to SonarCloud by using its REST API.
+
+Copyright (C) 2019-2020, Razvan Raducu
+Licensed under the GNU Public License v3 (GPLv3)
+---------------------------------------------------------------------
 """
 
 # pip install requests
-import requests 
+import requests
 import json
 import sys
 import os
 
+
 def printUsage():
-	print("Usage:\n"+
-		"Execute it with command \"./PythonSonarBot.py\". Make sure it has execute permission.\n"+
-		"The script receives the following arguments: \n"+
-		"\tThe directory you want the sourcecode to be downloaded in. Example: ./PythonSonarBot.py ./DataSet\n"+
-		"The script receives the following options: \n"+
-		"\tNo option. When no option is specified it will execute in quiet mode.\n"+
-		"\t-v option is used for verbose mode. Example: ./PythonSonarBot ./DataSet -v\n")
-	sys.exit()
+    print("Usage:\n" +
+          "Execute it with command \"./SVCP4C.py\". Make sure it has execute permission.\n" +
+          "The script receives the following arguments: \n" +
+          "\tThe directory you want the source code to be downloaded in. Example: ./SVCP4C.py ./DataSet\n" +
+          "The script receives the following options: \n" +
+          "\tNo option. When no option is specified it will execute in quiet mode.\n" +
+          "\t-v option is used for verbose mode. Example: ./SVCP4C ./DataSet -v\n")
+    sys.exit()
+
 
 def checkPath(path):
-	if not os.path.exists(path):
-		try:
-			os.makedirs(path)
-		except OSError as err:
-			print("Error: {0}".format(err))
-			sys.exit()
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except OSError as err:
+            print("Error: {0}".format(err))
+            sys.exit()
 
 
 ############################↓↓↓ Detecting arguments and options ↓↓↓######################################
-print("#### Append -h to print usage. (./PythonSonarBot.py -h) ####\n")
+print("#### Append -h to print usage. (./SVCP4C.py -h) ####\n")
 verbose = 0
 dumpDir = ""
 if len(sys.argv) == 2:
-	if sys.argv[1] == '-h':
-		printUsage()
-	else:
-		dumpDir = sys.argv[1]
-		checkPath(dumpDir)
-elif len(sys.argv) == 3: 
-	if sys.argv[2] == '-v': verbose +=1 
-	else:
-		print("Wrong usage. Aborting")
-		sys.exit() 
-	dumpDir = sys.argv[1]
-	checkPath(dumpDir)
+    if sys.argv[1] == '-h':
+        printUsage()
+    else:
+        dumpDir = sys.argv[1]
+        checkPath(dumpDir)
+elif len(sys.argv) == 3:
+    if sys.argv[2] == '-v':
+        verbose += 1
+    else:
+        print("Wrong usage. Aborting")
+        sys.exit()
+    dumpDir = sys.argv[1]
+    checkPath(dumpDir)
 else:
-	print("Wrong usage. Aborting")
-	sys.exit()
+    print("Wrong usage. Aborting")
+    sys.exit()
 
-print("#### Executing verbosely") if verbose else print("#### Executing in quiet mode. ####")
+print("#### Executing verbosely") if verbose else print(
+    "#### Executing in quiet mode. ####")
 
 verbosePrint = print if verbose else lambda k: None
 ############################↑↑↑ Detecting arguments and options ↑↑↑######################################
 
 ############################↓↓↓ Requesting project IDS ↓↓↓######################################
+
+
 def APIProjectRequest():
-	global remainingResults
-	global queryJsonResponse
+    global remainingResults
+    global queryJsonResponse
 
-	url = 'https://sonarcloud.io/api/components/search_projects'
-	parameters = {'filter':'security_rating>=2 and languages=c','p': p,'ps': ps }
+    url = 'https://sonarcloud.io/api/components/search_projects'
+    parameters = {
+        'filter': 'security_rating>=2 and languages=c', 'p': p, 'ps': ps}
 
-	try:
-		req = requests.get(url, params=parameters)
-	except requests.exceptions.RequestException as e:
-		print("Error: {0}".format(e))
-		sys.exit()
+    try:
+        req = requests.get(url, params=parameters)
+    except requests.exceptions.RequestException as e:
+        print("Error: {0}".format(e))
+        sys.exit()
 
-	verbosePrint("#### Request made to " + req.url + " ####")
+    verbosePrint("#### Request made to " + req.url + " ####")
 
-	# Writing the results of the query to a file
-	queryJsonResponse = req.json()
-	totalResults = queryJsonResponse['paging']['total']
-	verbosePrint("#### Query generated " + str(totalResults) + " results ####")
+    # Writing the results of the query to a file
+    queryJsonResponse = req.json()
+    totalResults = queryJsonResponse['paging']['total']
+    verbosePrint("#### Query generated " + str(totalResults) + " results ####")
 
-	verbosePrint("#### Writing page " + str(p) + " to file ####")
+    verbosePrint("#### Writing page " + str(p) + " to file ####")
 
-	# The writing is done in 'a' (append) mode (optional)
-	##print(json.dumps(queryJsonResponse, indent=4), file=open('sonarQueryResults.json','a'))
+    # The writing is done in 'a' (append) mode (optional)
+    ##print(json.dumps(queryJsonResponse, indent=4), file=open('sonarQueryResults.json','a'))
 
-	remainingResults = totalResults - (ps*p)
-	if remainingResults < 0:
-		remainingResults = 0
-	verbosePrint("#### There are " + str(remainingResults) + " left to #print ####")
+    remainingResults = totalResults - (ps*p)
+    if remainingResults < 0:
+        remainingResults = 0
+    verbosePrint("#### There are " + str(remainingResults) +
+                 " left to #print ####")
+
 
 p = 1
 ps = 500
@@ -94,11 +112,12 @@ queryJsonResponse = 0
 APIProjectRequest()
 
 while remainingResults > 500:
-	if p == 20: # 500 results * 20 pages = 10000 limit reached
-		break
-	p+=1
-	verbosePrint("#### Querying again. Requesting pageindex " + str(p) + " ####")
-	APIProjectRequest()
+    if p == 20:  # 500 results * 20 pages = 10000 limit reached
+        break
+    p += 1
+    verbosePrint("#### Querying again. Requesting pageindex " +
+                 str(p) + " ####")
+    APIProjectRequest()
 
 
 #################################↑↑↑  Requesting project IDS  ↑↑↑################################
@@ -111,51 +130,53 @@ https://sonarcloud.io/web_api/api/sources/raw. The key is ['issues']['component'
 value from the queryJsonResponse at this moment.
 """
 
+
 def APISourceCodeRequest():
-	url = 'https://sonarcloud.io/api/sources/raw'
+    url = 'https://sonarcloud.io/api/sources/raw'
 
-	# For each project ID, we get its source code and name it according to the following pattern:
-	# fileKey_startLine:endLine.c
+    # For each project ID, we get its source code and name it according to the following pattern:
+    # fileKey_startLine:endLine.c
 
-	with open('sonarQueryResults.json') as data_file:    
-		data = json.load(data_file)
+    with open('sonarQueryResults.json') as data_file:
+        data = json.load(data_file)
 
-	for issue in data['issues']:
-		fileKey = issue['component']
-		parameters = {'key':fileKey}
-		try:
-			req = requests.get(url, params=parameters)
-		except requests.exceptions.RequestException as e:
-			print(e)
-			print("Aborting")
-			sys.exit(1)
+    for issue in data['issues']:
+        fileKey = issue['component']
+        parameters = {'key': fileKey}
+        try:
+            req = requests.get(url, params=parameters)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            print("Aborting")
+            sys.exit(1)
 
-		# If the file contains errors because it was not found, we simply skip it.
-		if req.content.find(b"{\"errors\":[{\"msg\":") != -1:
-			print("#### FILE " + req.url + " SKIPPED BECAUSE IT CONTAINS ERRORS ####\n")
-			print(req.content)
-			print("######################\n")
-			continue
+        # If the file contains errors because it was not found, we simply skip it.
+        if req.content.find(b"{\"errors\":[{\"msg\":") != -1:
+            print("#### FILE " + req.url +
+                  " SKIPPED BECAUSE IT CONTAINS ERRORS ####\n")
+            print(req.content)
+            print("######################\n")
+            continue
 
-		# We replace '/' with its hex value 2F
-		vulnerableFile = (str(dumpDir)+"/"+(str(fileKey)).replace('/','2F'))
-		verbosePrint("Looking if "+ vulnerableFile+ " exists.")
-		if not os.path.isfile(vulnerableFile):
-			verbosePrint("++++> File doesn't exist. Creating <++++")
-			with open(vulnerableFile, 'ab+') as file:
-				file.write(req.content)
-				file.write(str.encode("//\t\t\t\t\t\t↓↓↓VULNERABLE LINES↓↓↓\n\n"))
-				file.write(str.encode("// " + str(issue['textRange']['startLine']) + "," + str(issue['textRange']['startOffset']) + ";" + str(issue['textRange']['endLine'])+ "," +str(issue['textRange']['endOffset']) +"\n\n"))
-		else:
-			verbosePrint("----> File exists. Appending vulnerable lines <----")
-			with open(vulnerableFile, 'ab+') as file:
-				file.write(str.encode("// "+ str(issue['textRange']['startLine']) + "," +  str(issue['textRange']['startOffset']) + ";" + str(issue['textRange']['endLine'])+ "," +str(issue['textRange']['endOffset']) +"\n\n"))
+        # We replace '/' with its hex value 2F
+        vulnerableFile = (str(dumpDir)+"/"+(str(fileKey)).replace('/', '2F'))
+        verbosePrint("Looking if " + vulnerableFile + " exists.")
+        if not os.path.isfile(vulnerableFile):
+            verbosePrint("++++> File doesn't exist. Creating <++++")
+            with open(vulnerableFile, 'ab+') as file:
+                file.write(req.content)
+                file.write(str.encode(
+                    "//\t\t\t\t\t\t↓↓↓VULNERABLE LINES↓↓↓\n\n"))
+                file.write(str.encode("// " + str(issue['textRange']['startLine']) + "," + str(issue['textRange']['startOffset']) + ";" + str(
+                    issue['textRange']['endLine']) + "," + str(issue['textRange']['endOffset']) + "\n\n"))
+        else:
+            verbosePrint("----> File exists. Appending vulnerable lines <----")
+            with open(vulnerableFile, 'ab+') as file:
+                file.write(str.encode("// " + str(issue['textRange']['startLine']) + "," + str(issue['textRange']['startOffset']) + ";" + str(
+                    issue['textRange']['endLine']) + "," + str(issue['textRange']['endOffset']) + "\n\n"))
 
-		
-		
 
 ##################################↑↑↑ Requesting sourcecode ↑↑↑##################################
-
 #################################↓↓↓ Requesting vulnerabilities ↓↓↓##############################
 """
 Here are the keys of every single repo that meets the following conditions:
@@ -163,10 +184,10 @@ Here are the keys of every single repo that meets the following conditions:
 	2. Is written in C language
 	3. Its security rating is >= 2
 """
-projectIds = "" 
+projectIds = ""
 for component in queryJsonResponse['components']:
-	# It's appended into the list to compose the following request.
-	projectIds += str(component['key']) + ","
+    # It's appended into the list to compose the following request.
+    projectIds += str(component['key']) + ","
 
 # Deletion of trailing comma. (Right side of index specifier is exclusive)
 projectIds = projectIds[:-1]
@@ -174,44 +195,49 @@ projectIds = projectIds[:-1]
 p = 1
 remainingResults = 0
 
+
 def APIVulnsRequest():
-	global remainingResults
-	url = 'https://sonarcloud.io/api/issues/search'
-	parameters = {'projects':projectIds, 'types':'VULNERABILITY', 'languages':'c', 'ps':ps, 'p': p }
+    global remainingResults
+    url = 'https://sonarcloud.io/api/issues/search'
+    parameters = {'projects': projectIds,
+                  'types': 'VULNERABILITY', 'languages': 'c', 'ps': ps, 'p': p}
 
-	try:
-		req = requests.get(url, params=parameters)
-	except requests.exceptions.RequestException as e:
-		print(e)
-		print("Aborting")
-		sys.exit(1)
+    try:
+        req = requests.get(url, params=parameters)
+    except requests.exceptions.RequestException as e:
+        print(e)
+        print("Aborting")
+        sys.exit(1)
 
-	verbosePrint("#### Request made to " + req.url + " ####")
+    verbosePrint("#### Request made to " + req.url + " ####")
 
-	# Writing the results of the query to a file
-	queryJsonResponse = req.json()
-	print(json.dumps(queryJsonResponse, indent=4), file=open('sonarQueryResults.json','w'))
+    # Writing the results of the query to a file
+    queryJsonResponse = req.json()
+    print(json.dumps(queryJsonResponse, indent=4),
+          file=open('sonarQueryResults.json', 'w'))
 
-	## REQUESTING SOURCECODE ##
-	verbosePrint("#### REQUESTING SOURCECODE ####")
-	APISourceCodeRequest()
+    ## REQUESTING SOURCECODE ##
+    verbosePrint("#### REQUESTING SOURCECODE ####")
+    APISourceCodeRequest()
 
-	totalResults = queryJsonResponse['total']
-	verbosePrint("#### Query generated " + str(totalResults) + " results ####")
+    totalResults = queryJsonResponse['total']
+    verbosePrint("#### Query generated " + str(totalResults) + " results ####")
 
+    remainingResults = totalResults - (ps*p)
+    if remainingResults < 0:
+        remainingResults = 0
+    verbosePrint("#### There are " + str(remainingResults) +
+                 " left to print ####")
 
-	remainingResults = totalResults - (ps*p)
-	if remainingResults < 0:
-		remainingResults = 0
-	verbosePrint("#### There are " + str(remainingResults) + " left to print ####")
 
 APIVulnsRequest()
 
 while remainingResults > 500:
-	if p == 20: # 500 results * 20 pages = 10000 limit reached
-		break
-	p+=1
-	verbosePrint("#### Querying again. Requesting pageindex " + str(p) + " ####")
-	APIVulnsRequest()
+    if p == 20:  # 500 results * 20 pages = 10000 limit reached
+        break
+    p += 1
+    verbosePrint("#### Querying again. Requesting pageindex " +
+                 str(p) + " ####")
+    APIVulnsRequest()
 
 ###############################↑↑↑ Requesting vulnerabilities ↑↑↑################################
